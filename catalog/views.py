4b5e1cb572c32f72dd30d375d7109f62d1b7aee8
ChiVN-sun-asstesterisk <ChiVN-sun-asterisk>
2020-08-11 14:02:21 +0700

    Part 6: Generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
   """View function for home page of site."""
@@ -29,3 +30,39 @@ def index(request):
 
   # Render the HTML template index.html with the data in the context variable
   return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+  model = Book
+  paginate_by = 10
+
+  def get_context_data(self, **kwargs):
+    # Call the base implementation first to get the context
+    context = super(BookListView, self).get_context_data(**kwargs)
+    # Create any data and add it to the context
+    context['some_data'] = 'This is just some data'
+    return context
+
+class BookDetailView(generic.DetailView):
+  model = Book
+
+  def book_detail_view(request, primary_key):
+    book = get_object_or_404(Book, pk=primary_key)
+    return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class AuthorListView(generic.ListView):
+  model = Author
+  paginate_by = 10
+
+  def get_context_data(self, **kwargs):
+    # Call the base implementation first to get the context
+    context = super(AuthorListView, self).get_context_data(**kwargs)
+    # Create any data and add it to the context
+    context['some_data'] = 'This is just some data'
+    return context
+
+class AuthorDetailView(generic.DetailView):
+  model = Author
+
+  def author_detail_view(request, primary_key):
+    author = get_object_or_404(Author, pk=primary_key)
+    return render(request, 'catalog/author_detail.html', context={'author': author})
